28af14f5c15de046342ccaefa2451023ee244b41
Benkő Pál <benko.pal90@gmail.com>
2023-10-11 17:11:47 +0200

    test formality file

---
+++ cheat-sheet/src/MethodName_StateUnderTest_ExpectedBehavior.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class MethodName_StateUnderTest_ExpectedBehavior {
+
+    @Test
+    public void myMethodName_WithRandomNumbers_ShouldReturnFalse(){
+        //ASSERT ACTION
+    }
+}
