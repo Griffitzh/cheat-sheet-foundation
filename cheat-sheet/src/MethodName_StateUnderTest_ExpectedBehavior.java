948be10cee0afd357a610fbd5006b0cec442b7c1
Benkő Pál <benko.pal90@gmail.com>
2023-10-11 17:14:50 +0200

    test formality file

--- cheat-sheet/src/MethodName_StateUnderTest_ExpectedBehavior.java
+++ cheat-sheet/src/MethodName_StateUnderTest_ExpectedBehavior.java
@@ -1,10 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class MethodName_StateUnderTest_ExpectedBehavior {
 
+    Anagram anagram;
+
+    //create new Class INSTANCE BEFORE EVERY TEST
+    @BeforeEach
+    void setup(){
+        anagram = new Anagram();
+    }
+
     @Test
+    @DisplayName("myMethodName width random numbers should return false")
     public void myMethodName_WithRandomNumbers_ShouldReturnFalse(){
         //ASSERT ACTION
     }
